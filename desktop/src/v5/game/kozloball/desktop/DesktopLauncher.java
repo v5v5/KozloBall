f4d23016e91a193d98a550fd90469ff0a9d9db11
v5 <v5@hotbox.ru>
2015-07-01 08:17:35 +0300

    start

---
+++ desktop/src/v5/game/kozloball/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package v5.game.kozloball.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import v5.game.kozloball.KozloBallGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KozloBallGame(), config);
+	}
+}
